a2f4b9484d9e34b831a87069ba8143f762564ce5
khuysor <huysorkheang22@gmail.com>
2024-05-03 15:25:56 +0700

    mapping one to many relationship cate->course

--- src/main/java/com/huysor/projectschool/mapping/CategoryMapper.java
+++ src/main/java/com/huysor/projectschool/mapping/CategoryMapper.java
@@ -1,13 +1,27 @@
 package com.huysor.projectschool.mapping;
 
-import com.huysor.projectschool.dto.CategoryDTO;
+import com.huysor.projectschool.dto.category.CategoryPostDTO;
+import com.huysor.projectschool.dto.category.CategoryRequestDTO;
+import com.huysor.projectschool.dto.category.CategoryWithCourse;
 import com.huysor.projectschool.entity.Category;
+import com.huysor.projectschool.entity.Course;
+import com.huysor.projectschool.services.CategoryServices;
+import com.huysor.projectschool.services.CourseServices;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
-    CategoryMapper INSTANCE= Mappers.getMapper(CategoryMapper.class);
-    CategoryDTO toCategoryDTO(Category category);
-    Category toCategory(CategoryDTO categoryDTO);
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryRequestDTO toCateRequest(Category category);
+
+    @Mapping(target = "id", ignore = true)
+    Category toCategoryPost(CategoryPostDTO categoryPostDTO);
+
+    @Mapping(target = "coursesList", source = "courses")
+    CategoryWithCourse toCategoryWithCourse(Category category);
 }
