9970c0cc440969b4dc7145ce4c2cc0905cc4a8e1
khuysor <huysorkheang22@gmail.com>
2024-05-09 20:52:20 +0700

    optimize import

--- src/main/java/com/huysor/projectschool/dto/category/CategoryRequestDTO.java
+++ src/main/java/com/huysor/projectschool/dto/category/CategoryRequestDTO.java
@@ -1,15 +1,8 @@
 package com.huysor.projectschool.dto.category;
 
-import com.huysor.projectschool.dto.course.CourseRequestDTO;
-import com.huysor.projectschool.entity.Course;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 public class CategoryRequestDTO {
