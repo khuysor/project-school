706e9b49c166806613f481b5f03eac96f0695cc3
khuysor <huysorkheang22@gmail.com>
2024-05-22 23:25:20 +0700

    add cor to proejct

---
+++ src/main/java/com/huysor/projectschool/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.huysor.projectschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+
+    }
+}
