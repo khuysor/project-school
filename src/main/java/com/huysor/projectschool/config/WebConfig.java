4f365be17f160e1bec0d81a76f5202fad4946791
Huysor kheang <lokmjassor@gmail.com>
2024-10-31 21:50:28 +0700

    add cors

--- src/main/java/com/huysor/projectschool/config/WebConfig.java
+++ src/main/java/com/huysor/projectschool/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.huysor.projectschool.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
