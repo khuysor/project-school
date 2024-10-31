e0f07d4f64d1239fa243bbe24aa2f8bc9234b2af
Huysor kheang <lokmjassor@gmail.com>
2024-10-31 21:51:31 +0700

    change exception handler

--- src/main/java/com/huysor/projectschool/exception/ApiExceptionHandler.java
+++ src/main/java/com/huysor/projectschool/exception/ApiExceptionHandler.java
@@ -1,20 +1,16 @@
 package com.huysor.projectschool.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.huysor.projectschool.dto.response.ResultResp;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(
             value = {ApiRequestException.class}
     )
-    public ResponseEntity<Object> handleApiException(ApiRequestException exception) {
-        ApiException apiException = new ApiException(exception.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    public ResultResp handleApiException(ApiRequestException exception) {
+        return ResultResp.fail();
     }
+
 }
